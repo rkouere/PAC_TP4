6cbfd73e9a7a49ad41e68667754732624e799c24
rkouere <rkouere@gmail.com>
2015-04-14 21:52:49 +0200

    avancement sur shifumi

--- shifumi-deathmatch/tp.py
+++ shifumi-deathmatch/tp.py
@@ -1,23 +1,50 @@
 import client
 import elgamal
+import base64
 
 
+
+
+# valeur = hexa
+# fini par bit à 1
+pierre  = "PIERRE"
+feuille = "FEUILLE"
+# fini par bit à 0
+ciseau  = "CISEAU"
 
 
 URL="http://pac.bouillaguet.info/TP4"
-link_param = "/shifumi-deathmatch/insert-coin/echallier"
+start_game = "/shifumi-deathmatch/insert-coin/echallier"
 serverObj = client.Server(URL)
 
 gage = "/shifumi-deathmatch/move"
 
-roundStarter = "/shifumi-deathmatch/start/echallier"
+play_round = "/shifumi-deathmatch/start/echallier"
+
 
 
 # we start the round
-#serverObj.query(link_param)
+serverObj.query(start_game)
 
-#play = serverObj.query(roundStarter)
+play = serverObj.query(play_round)
 
-print(elgamal.generate_keys())
+# checks wether we have a commitment or a waiting status
+try:
+    if(play['commitment']):
+        ciphertext = play['ciphertext']
+        PK = play['PK']
+        print("com")
+except:
+    print("status")
+
+foobar = play['foobar']
+print(play)
+
+myElGamal = elgamal.generate_keys()
+print(myElGamal)
+myCryptedMsg = elgamal.encrypt(myElGamal['publicKey'], feuille)
+print(myCryptedMsg)
+
+
 
 # elGamal.ElGamal.generate(1024, Random.new().read)
